9bdd3b0f1b5f8fb2faa16f34a9c85c954f50703b
shoFrontend <shoFrontend@gmail.com>
2020-05-14 23:08:10 +0900

    サイドバーを読み込み路に再配置

--- htdocs/js/sidebar_fixed.js
+++ htdocs/js/sidebar_fixed.js
@@ -1,7 +1,7 @@
 // pc sidebar固定
 if (!goSP) {
   // PCの処理
-  $(window).on('load', function() {
+  $(window).on('load scroll', function() {
     var fix = $(".js_fixed_target_content");
     var main = $(".js_main_content");
     var targetHei = fix.height();
@@ -9,46 +9,44 @@ if (!goSP) {
     var fixedContent = main.outerHeight();
     var endline = fixedContent + mainContent - targetHei;
 
-    $(window).scroll(function() {
-      var scrollY = $(window).scrollTop();
+    var scrollY = $(window).scrollTop();
 
-      // ターゲットに到達していない時
-      if(scrollY <= mainContent){
-        fix.css({
-          "position": "relative",
-          "top" : "auto",
-          "bottom" : "auto",
-          "left" : 0
-        });
-        main.css({
-          "margin-left": 0
-        });
-      // ターゲットに到達している時
-      } else if (scrollY > mainContent && scrollY < endline) {
-        var $wrap = $(".main");
-        var wrapOffsetLeft = $wrap.offset().left;
-        var widFixContent = fix.width();
-        fix.css({
-          "position": "fixed",
-          "top": 0,
-          "bottom": "auto",
-          "left": wrapOffsetLeft
-        });
-        main.css({
-          "margin-left": widFixContent + 50
-        });
-      // ターゲットが範囲外になる時
-      } else {
-        fix.css({
-          "position": "absolute",
-          "top" : "auto",
-          "bottom" : 0,
-          "left" : 0
-        });
-        main.css({
-          "margin-left": widFixContent + 50
-        });
-      }
-    });
+    // ターゲットに到達していない時
+    if(scrollY <= mainContent){
+      fix.css({
+        "position": "relative",
+        "top" : "auto",
+        "bottom" : "auto",
+        "left" : 0
+      });
+      main.css({
+        "margin-left": 0
+      });
+    // ターゲットに到達している時
+    } else if (scrollY > mainContent && scrollY < endline) {
+      var $wrap = $(".main");
+      var wrapOffsetLeft = $wrap.offset().left;
+      var widFixContent = fix.width();
+      fix.css({
+        "position": "fixed",
+        "top": 0,
+        "bottom": "auto",
+        "left": wrapOffsetLeft
+      });
+      main.css({
+        "margin-left": widFixContent + 50
+      });
+    // ターゲットが範囲外になる時
+    } else {
+      fix.css({
+        "position": "absolute",
+        "top" : "auto",
+        "bottom" : 0,
+        "left" : 0
+      });
+      main.css({
+        "margin-left": widFixContent + 50
+      });
+    }
   });
 }
